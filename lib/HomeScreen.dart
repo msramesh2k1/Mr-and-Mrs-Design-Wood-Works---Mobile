621b425bc14272415dc628dec4c39d9d2754906c
Ramesh M S <msramesh2k1@gmail.com>
2022-01-21 19:47:53 +0530

    feat:Logo Widget

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mr_and_mrs/Widgets/Responsive_widget.dart';
+
+import 'constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -11,10 +14,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget(
-      desktop: desktop(),
-      tab: tablet(),
-      mobile: mobile(),
+    return Scaffold(
+      body: ResponsiveWidget(
+        desktop: desktop(),
+        tab: tablet(),
+        mobile: mobile(),
+      ),
     );
   }
 
@@ -28,7 +33,57 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget mobile() {
     return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Logo(),
+          
+        ],
+      ),
       color: Colors.white,
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Mr & Mrs",
+            style: GoogleFonts.novaSlim(
+              textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kBackgroundColor,
+                  fontSize: 25,
+                  letterSpacing: 0),
+            ),
+          ),
+          Text(
+            "Design Wood Works",
+            style: GoogleFonts.openSans(
+              textStyle: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  fontSize: 11,
+                  letterSpacing: 1),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
